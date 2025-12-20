d371d4f841ebaa211ec4d04fbbf97c2c7e2f126f
Acil <acilhamieh4@gmail.com>
2025-12-20 22:57:03 +0100

    JavaFX basic UI working

--- src/SimpleUI.java
+++ src/SimpleUI.java
@@ -1,4 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class SimpleUI  {
+public class SimpleUI extends Application {
 
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello AP4B");
+        Button button = new Button("Click me");
+
+        button.setOnAction(e -> {
+            label.setText("Button clicked!");
+        });
+
+        VBox root = new VBox(10);
+        root.getChildren().addAll(label, button);
+
+        Scene scene = new Scene(root, 300, 200);
+
+        stage.setTitle("Simple UI Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
